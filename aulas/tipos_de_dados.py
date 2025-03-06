83c8f0b5bf75cdbee5ff9c84320d47bca4cbca8b
Andressa-Madureira <andressa.olmadureira@gmail.com>
2025-03-06 19:31:55 -0300

    Praticando aula python básico I

---
+++ aulas/tipos_de_dados.py
@@ -0,0 +1,27 @@
+idade = 30
+type(idade)
+numero = 3.1514
+nome = 'Andressa'
+type(numero)
+sobrenome = 'Madureira'
+print(nome, sobrenome ,sep= "\n")
+resultado = input("Digite seu nome") 
+print("O nome do usuário é:" , resultado)
+idade_int = float(input("Digite sua idade"))
+print("O usuário tem: ", idade_int, "anos")
+
+5+5
+10/2
+3*8
+20-8
+10//8
+10 % 7
+#esse numero é impar 
+65%2
+#esse numero é par
+64%2
+a = 11
+b = 4
+print(a % b)
+
+print((3+6*2)/5)
