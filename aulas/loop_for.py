94df5e913a408456683c77a209f6d48fce393bb1
Andressa-Madureira <andressa.olmadureira@gmail.com>
2025-03-10 15:28:22 -0300

    Incluindo + 1 exemplo de range

--- aulas/loop_for.py
+++ aulas/loop_for.py
@@ -6,10 +6,13 @@ for elemento in list:
     print(elemento, elemento*2, elemento/2)
 
 
-#range - recebe 3 argumentos -> pesquisar melhor sobre range
+#range - recebe 3 argumentos -> é um sequência de N elementos que começa em 0
 list_num = [4,7,13,15,1,8,16,20,3]
 len(list_num)
 
 for indice in range(9):
     print(indice, list_num[indice])
-    
+
+N=5
+for i in range(N):
+    print("Olá,mundo")
