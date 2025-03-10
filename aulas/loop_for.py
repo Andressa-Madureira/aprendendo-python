9261ec07504a1abb18ecf4087052c071bf738f3e
Andressa-Madureira <andressa.olmadureira@gmail.com>
2025-03-10 15:25:54 -0300

    Inclusão de aulas/práticas

--- aulas/loop_for.py
+++ aulas/loop_for.py
@@ -1,4 +1,4 @@
-#percorre a lista
+#percorre cada elemento da lista
 
 list = [10, 8, 65, 24, 32]
 
@@ -6,3 +6,10 @@ for elemento in list:
     print(elemento, elemento*2, elemento/2)
 
 
+#range - recebe 3 argumentos -> pesquisar melhor sobre range
+list_num = [4,7,13,15,1,8,16,20,3]
+len(list_num)
+
+for indice in range(9):
+    print(indice, list_num[indice])
+    
