d64b76b6ca0a60cc516af9d24380ec887b4192b5
Andressa-Madureira <andressa.olmadureira@gmail.com>
2025-04-04 08:57:24 -0300

    Aprendendo abrir arquivo txt  em Python

---
+++ aulas/arquivos.py
@@ -0,0 +1,9 @@
+arquivo = open("ola_mundo.txt", "w", encoding="utf-8")
+
+#Para escrever no arquivo utilizamos o método write()
+arquivo.write("olá, pessoal\n Esse é o meu primeiro arquivo em Python")
+
+#Para fechar e salvar os arquivos:
+arquivo.close()
+
+
