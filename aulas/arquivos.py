bd6323f49ef2130eafc8575f22a41f8d135c6baa
Andressa-Madureira <andressa.olmadureira@gmail.com>
2025-04-08 20:05:47 -0300

    Arquivos em Python txt

--- aulas/arquivos.py
+++ aulas/arquivos.py
@@ -6,4 +6,21 @@ arquivo.write("olá, pessoal\n Esse é o meu primeiro arquivo em Python")
 #Para fechar e salvar os arquivos:
 arquivo.close()
 
+#with garante que o arquivo será aberto e fechado
+
+with open("ola_mundo4.txt", "r", encoding="utf-8") as terceiroArquivo:
+
+    terceiroArquivo.write("olá, devsss!\n Terceiro arquivo")
+
+
+#continuar escrevendo no nosso arquivo
+with open("ola_mundo4.txt", "a", encoding="utf-8") as terceiroArquivo:
+
+    terceiroArquivo.write("olá, devsss!, como vocês estão?")
+
+#lê todo o conteudo 
+with open("ola_mundo.txt", "r", encoding="utf-8") as f:
+   conteudo = f.read()
+
+#conteud.split("\n") pular a linha
 
