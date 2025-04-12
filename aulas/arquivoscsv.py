e18d6d8b562b97bae3eafa1a4037fa4f2b905bea
Andressa-Madureira <andressa.olmadureira@gmail.com>
2025-04-12 20:15:51 -0300

    Incluir o input e mostrar o resultado

--- aulas/arquivoscsv.py
+++ aulas/arquivoscsv.py
@@ -10,17 +10,21 @@ tabela = [["Aluno", "Nota 1", "Nota 2", "Presença"],
 with open("alunos_stars.csv","w", encoding="utf-8", newline='') as f:
 
     csv.writer(f, delimiter= ",", lineterminator="\n").writerows(tabela)
-
-
+    
+    
 #Como ler esse arquivo?
-
+#Leitura do arquivo, na forma de tabela
 with open("alunos_stars.csv", "r", encoding="utf-8" ) as f:
-    aluno = "Han"
-    
     tabela_lida = [linha for linha in csv.reader(f,delimiter= ",", lineterminator="\n")]
+#Processamento do arquivo
+    nome_alunos = [linha[0] for linha in tabela_lida]
     
-    dados_aluno = tabela_lida[[linha[0] for linha in tabela_lida].index(aluno)]
+    aluno = input(f"Digite o nome do(a) aluno(a) cuja média você quer saber?\n Opções: {nome_alunos[1:]}")
+    
+    
+    dados_aluno = tabela_lida[nome_alunos.index(aluno)]
 
+    
     notas1 = float(dados_aluno[1])
     notas2 = float(dados_aluno[2])
 
